bf841f468bd2ba2019f65cf66ee2258760100274
qkitzero <qkitzero@gmail.com>
2025-12-11 12:52:28 +0900

    add: test (#4)

---
+++ internal/domain/rule/random_strategy_test.go
@@ -0,0 +1,47 @@
+package rule
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/qkitzero/combination-service/internal/domain/category"
+	"github.com/qkitzero/combination-service/internal/domain/element"
+)
+
+func TestRandomStrategySelect(t *testing.T) {
+	t.Parallel()
+	numElements := 5
+	elements := make([]element.Element, numElements)
+	for i := range numElements {
+		elementName, err := element.NewName(fmt.Sprintf("element%d", i+1))
+		if err != nil {
+			t.Fatalf("failed to create element name: %v", err)
+		}
+		elements[i] = element.NewElement(element.NewElementID(), elementName, []category.Category{}, time.Now())
+	}
+	tests := []struct {
+		name     string
+		success  bool
+		count    int
+		elements []element.Element
+	}{
+		{"success random strategy select", true, 3, elements},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			randomStrategy := newRandomStrategy()
+
+			_, err := randomStrategy.Select(tt.count, tt.elements)
+			if tt.success && err != nil {
+				t.Errorf("expected no error, but got %v", err)
+			}
+			if !tt.success && err == nil {
+				t.Errorf("expected error, but got nil")
+			}
+		})
+	}
+}
